f9e147205a3c893d770758216bab89975cb2d0df
fhk <215537936@qq.com>
2020-02-28 01:09:15 +0800

    初始化项目

---
+++ src/page/layout/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="joker-container">
+    <div class="joker-sidebar">
+      <sidebar/>
+    </div>
+    <div class="joker-main">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Main from './Main';
+import Sidebar from './Sidebar';
+export default {
+  components:{
+    Main,
+    sidebar
+  }
+}
+</script>
---
+++ src/page/product/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="joker-page-product">
+    product
+  </div>
+</template>
