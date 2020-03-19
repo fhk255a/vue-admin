1aceb107e9bc6aec3392ee1a25d32b8f267fcd7d
Joker <215537936@qq.com>
2020-03-20 01:27:04 +0800

    商品查询接口

--- src/api/product.js
+++ src/api/product.js
@@ -1,1 +1,34 @@
 import request from '@/lib/axios';
+const URL = '/product';
+function http(request) {
+  return new Promise((resolve, reject) => {
+    request.then(res => {
+      if (res.data.code == 200) {
+        resolve(res.data);
+      } else {
+        resolve(res.data);
+      }
+    }).catch(err => {
+      reject(err);
+    })
+  })
+}
+const PRODUCT = {
+  list(params){
+    let promise = request({
+      url: URL+'/list',
+      method: 'get',
+      params
+    });
+    return http(promise);
+  },
+  add(data){
+    let promise = request({
+      url: URL+'/add',
+      method: 'post',
+      data
+    });
+    return http(promise);
+  },
+}
+export default PRODUCT;
