707ff4feac2cc74a25320aba74dfee86f9f6ea6d
Joker <hongkai.fan@orderplus.com>
2020-04-20 09:42:10 +0800

    no message

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -6,8 +6,8 @@ import router from '@/router';
 
 // 创建axios
 const request = axios.create({
-  baseURL: 'http://localhost:3000',
-  // baseURL: 'http://127.0.0.1:3000',
+  // baseURL: 'http://localhot:3000',
+  baseURL: 'http://172.18.3.51:3000',
   // baseURL: 'http://project.fhk255.cn',
   withCredentials: true
 });
